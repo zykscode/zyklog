61d853255eb86efb5bcfb777804f5bf94adfd77f
zykcode <zyskson@gmail.com>
2024-05-24 22:48:33 +0100

    tired

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
 import { useTheme } from 'next-themes'
+import { Button } from './ui/button'
 
 const Sun = ({ className }) => (
   <svg
